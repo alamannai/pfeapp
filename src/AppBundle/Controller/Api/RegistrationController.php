d1da573ceffc8e363161cc50d4c2476c29fe0d79
majdzrg <majdzrg@gmail.com>
2018-04-05 21:59:35 +0100

    fix register api

--- src/AppBundle/Controller/Api/RegistrationController.php
+++ src/AppBundle/Controller/Api/RegistrationController.php
@@ -39,49 +39,45 @@ class RegistrationController extends Controller
         $normalizers = array(new ObjectNormalizer());
         $serializer = new Serializer($normalizers, $encoders);
 
-
-        $nom=$request->query->get('nom');
-        $prenom=$request->query->get('prenom');
-        $email=$request->query->get('email');
-        $password=$request->query->get('password');
-
-
-    
-        $citoyen = new Citoyen();
-        $citoyen->setNom($nom);
-        $citoyen->setPrenom($prenom);
-        $citoyen->setEmail($email);
-
-        $form = $this->createForm(CitoyenType::class, $citoyen);
-        $form->handleRequest($request);
- 
-        $encoder = $this->get('security.password_encoder');
-        $password = $encoder->encodePassword($citoyen, $citoyen->getPlainPassword());
-            
-
-        $citoyen->setRole('ROLE_CITOYEN');
-        $citoyen->setPassword($password);
-           
-        $em = $this->getDoctrine()->getManager();
-        $emailcheck = $em->getRepository('AppBundle:Citoyen')->findBy(['email' => $email]);
-
-        if ($emailcheck== null) {
-
-            $em->persist($citoyen);
-            $em->flush();
-
-            $rep =array(
-             'msg' => 'compte cree'
-             );
-        }else{
-            $rep =null;
-        }
+        // use request to read post params 
+        // query is for get request 
         
-       
+        $nom=$request->request->get('nom');
+        $prenom=$request->request->get('prenom');
+        $email=$request->request->get('email');
+        $password=$request->request->get('password');
 
-
-           
-
+        // add param control 
+        if($nom && $prenom && $email && $password){
+            $citoyen = new Citoyen();
+            $citoyen->setNom($nom);
+            $citoyen->setPrenom($prenom);
+            $citoyen->setEmail($email);
+            $form = $this->createForm(CitoyenType::class, $citoyen);
+            $form->handleRequest($request);
+            $encoder = $this->get('security.password_encoder');
+            $password = $encoder->encodePassword($citoyen, $citoyen->getPlainPassword());                
+            $citoyen->setRole('ROLE_CITOYEN');
+            $citoyen->setPassword($password);               
+            $em = $this->getDoctrine()->getManager();
+            $emailcheck = $em->getRepository('AppBundle:Citoyen')->findBy(['email' => $email]);
+            if ($emailcheck== null) {
+                $em->persist($citoyen);
+                $em->flush();
+                $rep =array('ok' => false, 'msg' => 'already existe');
+            }else{
+                $rep =array(
+                    'ok' => false,
+                    'msg' => 'already existe'
+                    );
+            }    
+        }
+        else{
+            $rep =array(
+                'ok' => false,
+                'msg' => 'param missing'
+                );
+        }
         $response = $serializer->serialize($rep, 'json');
         return new Response($response);
         
